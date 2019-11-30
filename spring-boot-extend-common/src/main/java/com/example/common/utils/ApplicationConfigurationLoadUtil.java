32ee6705b6f56997309693f04debafac1f5f146f
MingKaBum <704714211@qq.com>
2019-11-30 17:04:54 +0800

    1.添加apollo客户端依赖，为后续迁移配置至apollo配置中心做准备；

---
+++ spring-boot-extend-common/src/main/java/com/example/common/utils/ApplicationConfigurationLoadUtil.java
@@ -0,0 +1,11 @@
+package com.example.common.utils;
+
+/**
+ * @version 1.0
+ * @ClassName ApplicationConfigurationLoadUtil
+ * @Description 应用配置加载工具类，优先读取本地配置文件，后读取apollo配置中心属性
+ * @Author mingj
+ * @Date 2019/11/30 17:02
+ **/
+public class ApplicationConfigurationLoadUtil {
+}
