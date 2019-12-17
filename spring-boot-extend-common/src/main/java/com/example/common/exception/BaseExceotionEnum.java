32fffd19de0ef814e0bdc3fa8ff4751eac441d6a
MingKaBum <704714211@qq.com>
2019-12-17 23:09:25 +0800

    1.新增异常枚举类；

---
+++ spring-boot-extend-common/src/main/java/com/example/common/exception/BaseExceotionEnum.java
@@ -0,0 +1,39 @@
+package com.example.common.exception;
+
+/**
+ * @version 1.0
+ * @ClassName BaseExceotionEnum
+ * @Description 异常枚举类
+ * @Author mingj
+ * @Date 2019/12/17 22:44
+ **/
+public enum BaseExceotionEnum {
+
+    RESOURCE_LOAD_ERROR("ERR_001", "资源文件加载失败")
+    ;
+
+    private String code;
+
+    private String message;
+
+    BaseExceotionEnum(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
