2c0176d01277a6973f8e557cde44ac1d7ce8b41c
MingKaBum <704714211@qq.com>
2020-02-06 16:41:22 +0800

    1.增加一个默认的分页插件，且默认关闭；

--- spring-boot-extend-mybatis/src/main/java/com/example/mybatis/plugin/ExamplePlugin.java
+++ spring-boot-extend-mybatis/src/main/java/com/example/mybatis/plugin/ExamplePlugin.java
@@ -1,11 +1,16 @@
 package com.example.mybatis.plugin;
 
+import org.apache.ibatis.builder.StaticSqlSource;
 import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Field;
 import java.util.Properties;
 
 /**
@@ -16,15 +21,30 @@ import java.util.Properties;
  * @Date 2019/11/14 21:31
  **/
 @Intercepts({
-        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class})
+        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class,  Object.class, RowBounds.class, ResultHandler.class})
 })
 public class ExamplePlugin implements Interceptor {
+
+    private static final int MAPPED_STATEMENT_INDEX = 0;
+    private static final int PARAMETER_INDEX = 1;
+    private static final int ROW_BOUNDS_INDEX = 2;
 
     private static final Logger logger = LoggerFactory.getLogger(ExamplePlugin.class);
 
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
-        logger.info("【示范拦截器】启动");
+        Object[] args = invocation.getArgs();
+        RowBounds bounds = (RowBounds) args[ROW_BOUNDS_INDEX];
+        if (bounds == RowBounds.DEFAULT){
+            return invocation.proceed();
+        }
+        MappedStatement mappedStatement = (MappedStatement) args[MAPPED_STATEMENT_INDEX];
+        BoundSql boundSql = mappedStatement.getBoundSql(args[PARAMETER_INDEX]);
+        String sql = boundSql.getSql() + String.format(" Limit %d,%d", bounds.getOffset(), bounds.getLimit());
+//        bounds = RowBounds.DEFAULT;
+        Field field = mappedStatement.getClass().getDeclaredField("sqlSource");
+        field.setAccessible(true);
+        field.set(mappedStatement, new StaticSqlSource(mappedStatement.getConfiguration(), sql, boundSql.getParameterMappings()));
         return invocation.proceed();
     }
 
